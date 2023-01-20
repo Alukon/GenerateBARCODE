e2cfeea056ff91fa90a6194ff42d1100731f2ff7
Alukon <111476972+Alukon@users.noreply.github.com>
2023-01-20 14:02:12 +1000

    считыватель штрихкода

--- main.py
+++ main.py
@@ -4,7 +4,17 @@
 from barcode.writer import ImageWriter
 import barcode
 
-ean = barcode.get('ean13', '123456789101', writer=ImageWriter())
-filename = ean.save('ean13')
+# ean = barcode.get('ean13', '123456789101', writer=ImageWriter())
+# filename = ean.save('ean13')
+# генератор штрихкода------------------------------
+
+# pip install pyzbar
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+image = Image.open('Кот.png')
+decoded = decode(image)
+print(decoded[0].data.decode('utf-8'))
+# считыватель штрихкода------------------------------
 
 
